125c743d64f5d9e69bfb60ed23c99c9ea2b59a5c
Dick Tseng <562276119@qq.com>
2016-07-09 16:19:42 +0800

    test: change db config

--- database/dbpool.js
+++ database/dbpool.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql')
 
-mysqlPool = mysql.createPool( {
-    user: 'root',
-    password: 'qweasdzxc',
-    database: 'wego'
+mysqlPool = mysql.createPool({
+    user: 'admin',
+    password: 'password',
+    database: 'wego',
+    host: '119.29.235.26'
 });
 
 console.log('database connection pool set up');
