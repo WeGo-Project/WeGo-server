ba78e5467aa0926257da84d124d242ba4f0dd5ee
naifan <haokqgood@gmail.com>
2016-07-10 21:13:35 +0800

    init commit

--- database/dbpool.js
+++ database/dbpool.js
@@ -1,9 +1,12 @@
 var mysql = require('mysql')
 
 mysqlPool = mysql.createPool({
-    user: 'root',
-    password: 'root',
-    database: 'wego'
+    //user: 'root',
+    //password: 'root',
+    database: 'wego',
+    user: 'admin',
+    password: 'password',
+    host: '119.29.235.26'
 });
 
 console.log('database connection pool set up');
---
+++ routes/exercise_comment.js
@@ -0,0 +1,23 @@
+/**
+ * Created by var on 2016/7/10.
+ */
+var express = require('express');
+var router = express.Router();
+var targetDB = require('../database/exercise_comment.js');
+
+router.use(function(req, res, next) {
+    //TODO: 验证与鉴权代码代码
+    next();
+});
+router.post('/addcomment', function(req, res) {
+    targetDB.addcomment(req.body, function(result) {
+        res.json(result);
+    });
+});
+
+router.post('/query_comment', function(req, res) {
+    targetDB.query_comment(req.body, function(result) {
+        res.json(result);
+    });
+});
+module.exports = router;
