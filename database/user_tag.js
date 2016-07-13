813eec536425f1c9abbdb079e3f73be069162a24
Dick Tseng <562276119@qq.com>
2016-07-13 23:46:23 +0800

    modify user_tag update_user_tag api

--- database/user_tag.js
+++ database/user_tag.js
@@ -43,7 +43,15 @@ CurrentDB.update = function(query, callback) {
                     callback(result);
                     return;
                 } else if (rows.length <= 0) {
-                    console.error('Empty in querying user_tag by user_id');
+                    console.log('Empty in querying user_tag by user_id');
+                    var add_query = {
+                        user_id: query.user_id,
+                        tags_id: query.tag
+                    };
+                    CurrentDB.add(add_query, function(result) {
+                        result.result = KeyDefine.RESULT_SUCCESS;
+                        callback(result);
+                    });
                 } else {
                     count = 0;
                     for (index in rows) {
@@ -60,49 +68,14 @@ CurrentDB.update = function(query, callback) {
                             } else {
                                 count++;
                                 if (count === rows.length) {
-                                    var tags = query.tag.split('[')[1].split(']')[0].split(',');
-                                    count = 0;
-                                    for (index in tags) {
-                                        queryOption = {
-                                            sql: 'INSERT INTO ?? VALUES (?, ?)',
-                                            values: [KeyDefine.TABLE_NAME, tags[index], query.user_id],
-                                            timeout: 10000
-                                        };
-                                        connection.query(queryOption, function(err, rows) {
-                                            if (err) {
-                                                if (err.code === 'ER_DUP_ENTRY') {
-                                                    console.error('Error in adding %s: ' + err.code, KeyDefine.TABLE_NAME);
-                                                    result.result = KeyDefine.PARAM_DUP_ENTRY;
-                                                    callback(result);
-                                                    return;
-                                                } else if (err.code === 'ER_TRUNCATED_WRONG_VALUE_FOR_FIELD') {
-                                                    console.error('Error in adding %s: ' + err.code, KeyDefine.TABLE_NAME);
-                                                    result.result = KeyDefine.PARAM_WRONG_TYPE_FOR_COLUMNS;
-                                                    callback(result);
-                                                    return;
-                                                } else if (err.code === 'ER_NO_REFERENCED_ROW_2') {
-                                                    console.error('Error in adding %s: ' + err.code, KeyDefine.TABLE_NAME);
-                                                    result.result = KeyDefine.RESULT_NOT_REFERENCED_ROW;
-                                                    callback(result);
-                                                    return;
-                                                } else {
-                                                    console.error('Error in inserting %s: ' + err.code, KeyDefine.TABLE_NAME);
-                                                    callback(result);
-                                                    return;
-                                                }
-                                            } else if (rows.affectedRows === 0) {
-                                                console.error('Failed in add user tag');
-                                                result.result = KeyDefine.RESULT_ADD_FAILED;
-                                                callback(result);
-                                                return;
-                                            }
-                                            count++;
-                                            if (count === tags.length) {
-                                                result.result = KeyDefine.RESULT_SUCCESS;
-                                                callback(result);
-                                            }
-                                        });
-                                    }
+                                    var add_query = {
+                                        user_id: query.user_id,
+                                        tags_id: query.tag
+                                    };
+                                    CurrentDB.add(add_query, function(result) {
+                                        result.result = KeyDefine.RESULT_SUCCESS;
+                                        callback(result);
+                                    });
                                 }
                             }
                         });
